f9846b7a8b565226e3a69b102d28d8ede999b2c9
Junior-45 <ginelceralus@gmail.com>
2025-04-28 13:43:58 -0400

    Update set.js

--- set.js
+++ set.js
@@ -1,15 +1,15 @@
 const dotenv = require('dotenv');
 dotenv.config();
 module.exports = {
-  PREFIXE: process.env.PREFIXE || "",
-  NOM_OWNER: process.env.NOM_OWNER || "Ainz",
-  NUMERO_OWNER: process.env.NUMERO_OWNER || "",
-  MODE: process.env.MODE || "public",
+  PREFIXE: process.env.PREFIXE || "a",
+  NOM_OWNER: process.env.NOM_OWNER || "Dragon",
+  NUMERO_OWNER: process.env.NUMERO_OWNER || "50935623320",
+  MODE: process.env.MODE || "private",
   MENU: process.env.MENU || "https://i.ibb.co/ynx9QcZ/image.jpg",
-  SESSION_ID: process.env.SESSION_ID || "ovl",
+  SESSION_ID: process.env.SESSION_ID || "Ovl-MD_PzP0ui3k_SESSION-ID",
   LEVEL_UP: process.env.LEVEL_UP || "non",
-  STICKER_PACK_NAME: process.env.STICKER_PACK_NAME || "Wa-sticker",
-  STICKER_AUTHOR_NAME: process.env.STICKER_AUTHOR_NAME || "OVL-MD",
+  STICKER_PACK_NAME: process.env.STICKER_PACK_NAME || "Dragon",
+  STICKER_AUTHOR_NAME: process.env.STICKER_AUTHOR_NAME || "Dragon",
   DATABASE: process.env.DATABASE,
   RENDER_API_KEY: process.env.RENDER_API_KEY,
 };
